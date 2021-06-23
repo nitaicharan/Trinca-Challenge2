59b179c0cd1b0412c84ac0805cf25e02d9c3b54a
Nitai Charan <nitaicharan@gmail.com>
2021-06-23 00:00:00 -0300

    Created signin user to a barbecue

--- src/database/migrations/1628693945442-CreateDatabase.ts
+++ src/database/migrations/1628693945442-CreateDatabase.ts
@@ -1,7 +1,7 @@
 import {MigrationInterface, QueryRunner} from "typeorm";
 
-export class CreateDatabase1628653691678 implements MigrationInterface {
-    name = 'CreateDatabase1628653691678'
+export class CreateDatabase1628693945442 implements MigrationInterface {
+    name = 'CreateDatabase1628693945442'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "users" ("id" varchar PRIMARY KEY NOT NULL, "name" varchar NOT NULL, "email" varchar NOT NULL, "password" float NOT NULL, "created_at" datetime NOT NULL DEFAULT (datetime('now')))`);
--- src/services/BarbecueService.ts
+++ src/services/BarbecueService.ts
@@ -1,8 +1,8 @@
-import { getCustomRepository } from 'typeorm';
+import { getConnection, getCustomRepository } from 'typeorm';
 import { CreateRequestBodyDto } from '../dto/Barbecue/CreateRequestBodyDto';
 import { CreateRequestQueryDto } from '../dto/Barbecue/CreateRequestQueryDto';
 import { BarbecueRepository } from '../repositories/BarbecueRepository';
-import * as UserService from './UserService';
+import { Barbecue } from './../entities/Barbecue';
 
 export const save = async (params: CreateRequestBodyDto & CreateRequestQueryDto) => {
     const repository = getCustomRepository(BarbecueRepository);
@@ -12,19 +12,21 @@ export const save = async (params: CreateRequestBodyDto & CreateRequestQueryDto)
 
 export const findAll = async () => {
     const repository = getCustomRepository(BarbecueRepository);
-    return await repository.find();
+    return await repository.find({ relations: ['users'], });
 }
 
 export const findById = async (id: string) => {
     const repository = getCustomRepository(BarbecueRepository);
-    return await repository.findOne({ id });
+    return await repository.findOne({ id }, { relations: ['users'] });
 }
 
 export const signUser = async (params: { user_id: string, barbecue_id: string }) => {
-    const repository = getCustomRepository(BarbecueRepository);
     // TODO check if user already exist at a middleware
-    const user = await UserService.findById(params.user_id);
     // TODO check if barbecue already exist at a middleware
-    const barbecue = await repository.findOne({ id: params.barbecue_id });
-    return repository.save({ ...barbecue, users: barbecue.users ? [...barbecue.users, user] : [user] });
+    await getConnection()
+        .createQueryBuilder()
+        .relation(Barbecue, 'users')
+        .of(params.barbecue_id)
+        .add(params.user_id);
+
 }
