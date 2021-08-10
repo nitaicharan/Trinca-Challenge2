c85feb64cb7c99dca7661bf1192878a84620eaf1
Nitai Charan <nitaicharan@gmail.com>
2021-08-10 20:50:58 -0300

    Fixed cross

--- src/api.ts
+++ src/api.ts
@@ -1,11 +1,19 @@
-import express from 'express';
-import routers from './routers';
+import express, { Request, Response } from 'express';
 import './database';
+import routers from './routers';
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use((req: Request, res: Response, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', Boolean(true).toString());
+    next();
+});
 
 routers.forEach(router => app.use(router));
 
